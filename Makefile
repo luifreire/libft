efdf943746a5110740cd3c099c4f1ef1083a2544
Luiza Freire <lufee20@gmail.com>
2023-07-20 15:53:17 -0300

    begging

---
+++ Makefile
@@ -0,0 +1,28 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: luisanto <luisanto@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/07/20 14:54:29 by luisanto          #+#    #+#              #
+#    Updated: 2023/07/20 14:56:46 by luisanto         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME =
+CC = gcc -Wall -Werror -Wextra
+SRC =
+
+all: $(NAME)
+
+$(NAME):
+	$(CC) -o $(NAME) $(SRC)
+	
+clean:
+	rm -rf *.o
+
+fclean: clean
+	rm $(NAME)
+
+re: fclean all
---
+++ ft_substr.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luisanto <luisanto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 14:38:26 by luisanto          #+#    #+#             */
+/*   Updated: 2023/07/20 15:10:46 by luisanto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_substr(char const *s, unsigned int start, unsigned int size_t, unsigned int len)
+{
+	char	*dest;
+
+	start = 0;
+	len = size_t - start;
+	
+	while ()
+	{
+		
+	}
+}
