24d319c257f6084e09c5337e87f16c63bba5e4c3
Luiza Freire <lufee20@gmail.com>
2023-07-24 15:43:24 -0300

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -10,15 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_substr(char const *s, unsigned int start, unsigned int size_t, unsigned int len)
+#include<stdlib.h>
+#include<stddef.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*dest;
+  unsigned int  i;
+  unsigned int total;
 
 	start = 0;
-	len = size_t - start;
-	
-	while ()
+  i = 0;
+  total = *s;
+
+	if (len != 0 && s[i] && start < total)
 	{
-		
-	}
+    dest = malloc(sizeof(char) * (len) + sizeof(char));
+    while (start < len && (*(s + i) != '\0'))
+    {
+      start++;
+      i++;
+    }
+      return (dest);
+	}  
+  else
+    return (NULL);
+}
+
+#include<stdio.h>
+
+int main()
+{
+    char src[] = "help me dude";
+ 
+    int start = 2;
+    int end = 4;
+ 
+    char* dest = ft_substr(src, start, end);
+ 
+    printf("%s\n", dest);
+ 
+    return 0;
 }
