e6741339c5e352a4f8c40f16de7707aaefa71f5b
Luiza Freire <lufee20@gmail.com>
2023-07-24 17:14:04 -0300

    Create ft_strlen.c
    working!

---
+++ ft_strlen.c
@@ -0,0 +1,29 @@
+//42 header//
+
+int	ft_strlen(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+#include<stdio.h>
+
+int main()
+{
+  int count;
+  
+  char stuff[] = "get it baby girl";
+  
+  count = ft_strlen(stuff);
+
+  printf("%d\n", count);
+
+  return 0;
+}
